9379d39b527fda89b4d5ab6b86fdd081c373e985
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-04-22 21:23:19 +0545

    map()

---
+++ Day9/map.js
@@ -0,0 +1,7 @@
+const students = [
+  { name: 'Anamika', age: 18 },
+  { name: 'Smriti', age: 19 },
+  { name: 'Ragita', age: 17 }
+];
+const names = students.map(student => student.name);
+console.log(names); 
