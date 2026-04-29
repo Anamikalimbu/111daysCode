df7c5feec2bd4c7c318508e46f0fa39df7e196d2
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-04-29 20:56:33 +0545

    Js

---
+++ Day16/loop.js
@@ -0,0 +1,14 @@
+// for loop 
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+// While loop 
+let i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+// for each loop 
+let names = ["Ram", "Sita", "Gita"];
+
+names.forEach(n => console.log(n));
