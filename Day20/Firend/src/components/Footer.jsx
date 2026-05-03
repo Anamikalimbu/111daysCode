3644b07aebc6d0b6a7c1483bacd74d5a1a22afee
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-05-03 20:44:47 +0545

    Day20

---
+++ Day20/Firend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2026 Anamika Limbu | MERN 111 Days</p>
+    </footer>
+  );
+};
+
+export default Footer;
