a1265d1fb2e784b3b05ed359ce80c61cc7e894a2
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-05-27 18:26:04 +0545

    js

---
+++ Day44/module.js
@@ -0,0 +1,14 @@
+// Modules
+// Modules - Encapsulated Code (only share minimum)
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum) 
+
+
+const names = require('./02')
+const sayHI = require('./03')
+// console.log(names)
+
+
+sayHI('Anamika')
+sayHI(names.Anu)
+sayHI(names.Smriti)
