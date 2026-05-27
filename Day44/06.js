cf5dd6d0ee444afe953ce0712f4dcd41b62f8e2b
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-05-27 18:36:43 +0545

    js

---
+++ Day44/06.js
@@ -0,0 +1,16 @@
+const num1 = 20
+const num2 = 10
+
+function subValues() {
+  console.log(`the difference is : ${num1 - num2}`)
+}
+function mulValues() {
+  console.log(`the product is : ${num1 * num2}`)
+}
+function divValues() {
+  console.log(`the quotient is : ${num1 / num2}`)
+}
+
+subValues()
+mulValues()
+divValues()
