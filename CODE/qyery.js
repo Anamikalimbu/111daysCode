82d878f18c701af8c1d952fe2f79266d84a10775
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-05-28 18:17:46 +0545

    js

---
+++ CODE/qyery.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/search", (req, res) => {
+
+    const name = req.query.name;
+
+    res.send(`Searching for ${name}`);
+});
+
+app.listen(3000);
