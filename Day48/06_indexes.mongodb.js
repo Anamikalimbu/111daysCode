e9d0d22ef0b690e6aa29d934f53dcafcc0124430
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-06-01 13:39:28 +0545

    add initial index setup for sales collection in e-commerce database

---
+++ Day48/06_indexes.mongodb.js
@@ -0,0 +1,11 @@
+use ('e-commerce');
+
+// db.sales.find()
+
+// db.sales.getIndexes()
+
+// create an index on the quantity field
+// db.sales.createIndex({ quantity: 1 })
+
+db.sales.getIndexes()
+
