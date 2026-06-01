8680a8d7922a058d932354f765d799d17e9cca42
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-06-01 10:29:59 +0545

    update product tags for Wireless Mouse and comment out price update examples

--- Day48/03_update.mongodb.js
+++ Day48/03_update.mongodb.js
@@ -1,6 +1,19 @@
 use ('e-commerce');
 
+// Update Product Price
+// db.products.updateOne(
+//     { name: 'Wireless Mouse' },
+//     { $set: { price: 999 } }
+// );
+
+// Update Multiple Products
+// db.products.updateMany(
+//     { category: 'Electronics' },
+//     { $inc: { stock: 100 } }
+// );
+
+// add array of tags to a product
 db.products.updateOne(
     { name: 'Wireless Mouse' },
-    { $set: { price: 500 } }
+    { $push: { tags: 'mouse' } }
 );
