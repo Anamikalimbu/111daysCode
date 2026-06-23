0c7f79ac4afc2e09b5da56c756dfe3150ed68737
Anamika Limbu <135096572+Anamikalimbu@users.noreply.github.com>
2026-06-23 19:42:54 +0545

    feat: add initial data analysis script and student dataset

---
+++ Python/Day71/dataanalysis.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+
+#Creating a Pandas Series Manually
+math_grades = [78, 88, 67]
+grades_series = pd.Series(math_grades, name = "Math_Score")
+print("---Pandas Series---")
+print(grades_series)
+
+# Creating a small Pandas Data Frame manually from a Dictionary
+
+student_dict ={
+    "StudentID": ["S001", "S002", "S003"],
+    "Name": ["Aarav Sharma", "Neha Limbu", "Rohan Karki"],
+    "Course": ["Computing", "Computing", "Business"]
+}
+small_df = pd.DataFrame(student_dict)
+print("\n--- Pandas DataFrame ---")
+print(small_df)
+
+df_students = pd.read_csv("E:\\Drive D\\NodeJs\\Python\\Day71\\students_pandas_day4 (1).csv")
+print("Dataset loaded successfully")
+
+#1. Look at the first 10 records
+print("---Top 10 Row (.head)---")
+print(df_students.head(10))
+
+#2. Check the structural layout and data types
+df_students.info()
+
+#3. Generate summary statistics for numeric scores
+print("\n---Statistics Summary (.describe)---")
+print(df_students.describe())
